095299e8112a44ae7b4c1c8591bea62f11457803
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2026-01-31 13:17:25 +0200

    status system tick speed

--- 2026-gamejam/status_effects/scripts/status_system.gd
+++ 2026-gamejam/status_effects/scripts/status_system.gd
@@ -4,6 +4,8 @@ class_name StatusSystem
 signal take_damage(dmg)
 var statuses:Array
 
+var tick_length=0.5
+var tick_timer=0
 
 func apply_status(status):
 	print("append satatus")
@@ -12,7 +14,10 @@ func apply_status(status):
 
 func _process(delta: float) -> void:
 	for status_index in len(statuses):
-		statuses[status_index][0].on_frame(delta,self)
+		if(tick_timer<=0):
+			statuses[status_index][0].on_frame(tick_length,self)
+			tick_timer=tick_length
+		tick_timer-=delta
 		statuses[status_index][1]-=delta
 		if(statuses[status_index][1]<=0):
 			statuses.remove_at(status_index)
