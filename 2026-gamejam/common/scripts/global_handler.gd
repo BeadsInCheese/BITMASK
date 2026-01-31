6d447714be2327030ed13bac9f2fdf48422c2e09
Teemu Lehti <temeleh@pm.me>
2026-02-01 00:06:46 +0200

    Add options screen with audio bus controls

--- 2026-gamejam/common/scripts/global_handler.gd
+++ 2026-gamejam/common/scripts/global_handler.gd
@@ -1,5 +1,8 @@
 extends Node
 
+signal audio_mute_toggled(value: float)
+
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("quit"):
 		get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
@@ -9,5 +12,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		var master = AudioServer.get_bus_index("Master")
 		if AudioServer.get_bus_volume_linear(master) > 0:
 			AudioServer.set_bus_volume_linear(master, 0)
+			audio_mute_toggled.emit(0)
 		else:
 			AudioServer.set_bus_volume_linear(master, 1)
+			audio_mute_toggled.emit(1)
---
+++ 2026-gamejam/gui/mainmenu/scripts/mainmenu.gd
@@ -0,0 +1,17 @@
+class_name MainMenu
+extends Node2D
+
+func _on_go_to_game_pressed() -> void:
+	get_tree().change_scene_to_packed(preload("res://game.tscn"))
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+var options_scene
+
+
+func _on_options_pressed() -> void:
+	options_scene = preload("res://gui/mainmenu/scenes/options.tscn").instantiate()
+	add_child(options_scene)
