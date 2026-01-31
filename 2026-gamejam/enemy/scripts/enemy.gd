7f0adf92e1cd5ad61881fb581ac04045e25ba608
Eetu Rantala <rantala.eetu@gmail.com>
2026-01-31 13:42:44 +0200

    Modify enemy to be character controller

--- 2026-gamejam/enemy/scripts/enemy.gd
+++ 2026-gamejam/enemy/scripts/enemy.gd
@@ -1,16 +1,23 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 @export var stats : Resource
 var player
+@export var speed: float = 70.0
 
 func _ready() -> void:
 	player = find_parent("game").get_node("Player")
 
+
 func _physics_process(delta: float) -> void:
 	$NavigationAgent2D.target_position = player.position
+	
+	if $NavigationAgent2D.is_navigation_finished():
+		return
+	
 	var next_path = $NavigationAgent2D.get_next_path_position()
-	var motion = next_path - position
-	move_and_collide(motion.normalized())
+	var direction = (next_path - position).normalized()
+	velocity = direction * speed
+	move_and_slide()
 
 func take_damage(f: float):
 	get_node("HPSystem").take_damage(f)
