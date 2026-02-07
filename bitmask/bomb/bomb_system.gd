25f18b674c0d52e09e30983eeb5c074b49d97967
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2026-02-07 21:03:19 +0200

    bomb system

---
+++ bitmask/bomb/bomb_system.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var bomb_prefab = preload("res://bomb/scenes/bomb.tscn")
+
+var max_bombs
+var current_bombs = 1
+
+
+func drop_bomb():
+	if current_bombs < 1:
+		return
+	current_bombs -= 1
+	var bomb_instance = bomb_prefab.instantiate()
+	bomb_instance.global_position = global_position
+	get_tree().root.add_child(bomb_instance)
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("bomb"):
+		drop_bomb()
