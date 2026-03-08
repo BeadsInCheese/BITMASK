5697e1d8fae252c365cbfd2063622d1df5314386
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2026-03-08 19:12:59 +0200

    player is no longer immune to bombs

--- bitmask/bullet/modifiers/scripts/damage.gd
+++ bitmask/bullet/modifiers/scripts/damage.gd
@@ -2,8 +2,11 @@ class_name DirectDamage
 extends BulletModifier
 
 @export var amount: float = 10
+@export var player_is_immune: bool = false
 
 
 func on_collision(bullet, target):
-	if target.has_method("take_damage") and target is not Player:
+	if target is Player and player_is_immune:
+		return
+	if target.has_method("take_damage"):
 		target.take_damage(amount)
